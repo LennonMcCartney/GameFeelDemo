4d2609a559daf770c8cd60529d1ad612af8645e6
Lycia Stenson <66551408+LennonMcCartney@users.noreply.github.com>
2024-02-14 14:16:20 +0000

    Some changes

--- Scripts/ShadowProjector.gd
+++ Scripts/ShadowProjector.gd
@@ -1,10 +1,31 @@
-extends RayCast3D
+extends Node3D
+
+var shadow_projector_nodes : Array[Node]
+var shadow_projectors : Array[RayCast3D]
 
 @onready var shadow_decal : Decal = $ShadowDecal
 
+func _ready():
+	shadow_projector_nodes = $ShadowProjectors.get_children()
+	for shadow_projector_node in shadow_projector_nodes:
+		if shadow_projector_node is RayCast3D:
+			var shadow_projector : RayCast3D = shadow_projector_node
+			shadow_projectors.append(shadow_projector)
+		else:
+			printerr("ShadowProjectors child is not RayCast3D")
+
 func _process(_delta):
-	if is_colliding():
-		shadow_decal.global_position.y = get_collision_point().y
-		var new_scale : float = 1.0 / (2.0 + (0.5 * (global_position.y - shadow_decal.global_position.y)))
-		#print(new_scale)
-		shadow_decal.scale = Vector3(new_scale, new_scale, new_scale)
+	var position_y : float = 0
+	for shadow_projector : RayCast3D in shadow_projectors:
+		if shadow_projector.is_colliding():
+			var new_position_y : float = shadow_projector.get_collision_point().y
+			print(new_position_y)
+			if new_position_y > position_y:
+				position_y = new_position_y
+	shadow_decal.global_position.y = position_y
+		#print(shadow_projector)
+	#for shadow_projector
+	#if shadow_projector.is_colliding():
+		#shadow_decal.global_position.y = shadow_projector.get_collision_point().y
+		#var new_scale : float = 1.0 / (2.0 + (0.5 * (global_position.y - shadow_decal.global_position.y)))
+		#shadow_decal.scale = Vector3(new_scale, new_scale, new_scale)
