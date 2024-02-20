97b487a6a1319a5a125bce75b1a35a7c987ed5e8
Lycia Stenson <66551408+LennonMcCartney@users.noreply.github.com>
2024-02-20 15:52:20 +0000

    Removed need for sprint particles manager

--- Scripts/JumpParticlesManager.gd
+++ Scripts/JumpParticlesManager.gd
@@ -1,16 +1,8 @@
-extends Node3D
-
-@onready var jump_particles_packed_scene : PackedScene = preload("res://Scenes/Particles/JumpParticles.tscn")
-var jump_particles : GPUParticles3D
+extends GPUParticles3D
 
 func _ready():
-	jump_particles = jump_particles_packed_scene.instantiate()
-	add_child(jump_particles)
-	
 	GameManager.jump.connect(spawn_jump_particles)
 
 func spawn_jump_particles(spawn_position : Vector3):
-	#var jump_particles_instance : GPUParticles3D = jump_particles.instantiate()
-	jump_particles.global_position = spawn_position
-	jump_particles.emitting = true
-	#add_child(jump_particles_instance)
+	global_position = spawn_position
+	emitting = true
--- Scripts/SprintParticlesManager.gd
+++ Scripts/SprintParticlesManager.gd
@@ -1,18 +1,18 @@
-extends Node3D
+extends GPUParticles3D
 
-@onready var sprint_particles_packed_scene : PackedScene = preload("res://Scenes/Particles/SprintParticles.tscn")
-var sprint_particles : GPUParticles3D
+#@onready var sprint_particles_packed_scene : PackedScene = preload("res://Scenes/Particles/SprintParticles.tscn")
+#var sprint_particles : GPUParticles3D
 
 func _ready():
-	sprint_particles = sprint_particles_packed_scene.instantiate()
-	add_child(sprint_particles)
+	#sprint_particles = sprint_particles_packed_scene.instantiate()
+	#add_child(sprint_particles)
 	GameManager.sprint.connect(spawn_sprint_particles)
 
 func spawn_sprint_particles(spawn_position : Vector3, speed : float):
-	sprint_particles.global_position = spawn_position
-	if speed > 7.5:
-		print("emitting speed > ", speed)
-		sprint_particles.emitting = true
+	global_position = spawn_position
+	if speed > 8.0:
+		#print("emitting speed > ", speed)
+		emitting = true
 	else:
-		print("not emitting speed > ", speed)
-		sprint_particles.emitting = false
+		#print("not emitting speed > ", speed)
+		emitting = false
