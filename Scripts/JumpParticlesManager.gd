cd741c75b288b65162402da621ab986b8e73d796
Lycia Stenson <66551408+LennonMcCartney@users.noreply.github.com>
2024-02-23 17:41:07 +0000

    Renamed SprintParticlesManager to SprintParticles

--- Scripts/JumpParticlesManager.gd
+++
@@ -1,8 +0,0 @@
-extends GPUParticles3D
-
-func _ready():
-	GameManager.jump.connect(spawn_jump_particles)
-
-func spawn_jump_particles(spawn_position : Vector3):
-	global_position = spawn_position
-	emitting = true
